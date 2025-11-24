6fe73622af3629d138a536d58537329b8c3004e4
pope-h <hamatthan@gmail.com>
2025-11-24 20:25:24 +0100

    reorganized the interfaces

--- src/interfaces/IProfitManager.sol
+++ src/interfaces/IProfitManager.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.26;
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 
 interface IProfitManager {
-    function accrueProfit(PoolKey calldata poolKey, address lp, uint256 amount0, uint256 amount1) external;
-    function withdrawProfits(PoolKey calldata poolKey) external;
     function getLPProfits(PoolKey calldata poolKey, address lp)
         external
         view
         returns (uint256 profits0, uint256 profits1);
+
+    function accrueProfit(PoolKey calldata poolKey, address lp, uint256 amount0, uint256 amount1) external;
+    function withdrawProfits(PoolKey calldata poolKey) external;
     function checkAndExecuteAutoHedge(PoolKey calldata poolKey, address lp) external;
 }
